2e2f4532230ac9c720fa0fda50f21c21774ab1ca
Eric Jones <eric.web.email@gmail.com>
2021-03-15 08:05:31 -0400

    First Proposed Solution

--- src/App.js
+++ src/App.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...this.updateEquation(),
+      numQuestions: 0,
+      numCorrect: 0
+    }
+  }
+
+  getRandomValue(multiplier = 100) {
+    return Math.floor(Math.random() * multiplier);
+  }
+
+  checkAnswer(answer) {
+    let isCorrect = false;
+    if ((this.state.isEqual && answer) || (!this.state.isEqual && !answer)) {
+      isCorrect = true;
+    }
+    this.setState((currentState) => ({
+      numQuestions: ++currentState.numQuestions,
+      numCorrect: isCorrect ? ++currentState.numCorrect : currentState.numCorrect,
+      ...this.updateEquation(),
+    }));
+  }
+
+  updateEquation() {
+    const value1 = this.getRandomValue(100);
+    const value2 = this.getRandomValue(100);
+    const value3 = this.getRandomValue(100);
+    const proposedAnswer = this.getRandomValue(3) + value1 + value2 + value3;
+    const isEqual = proposedAnswer === (value1 + value2 + value3);
+    return {value1, value2, value3, isEqual, proposedAnswer}
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,12 +48,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={() => this.checkAnswer(true)}>True</button>
+          <button onClick={() => this.checkAnswer(false)}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
